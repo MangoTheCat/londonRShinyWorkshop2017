8efdc037032fc0db2be2b5ff2f6dd328ef49e6e9
Nick Howlett <nhowlett@mango-solutions.com>
2017-11-17 14:47:58 +0000

    removed exercises from git

--- exercises/1/server.R
+++
@@ -1,5 +0,0 @@
-shinyServer(function(input, output){
-  output$dateOutput <- renderText(
-    format(input$dateInput, format = "A %A in %B. The year is %Y")
-    )  
-})
--- exercises/2/server.R
+++
@@ -1,5 +0,0 @@
-shinyServer(function(input, output){
-  output$plotOutput <- renderPlot(
-    hist(rnorm(input$numberInput), col = input$colInput )
-  )
-})
